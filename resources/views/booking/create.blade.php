d54f88071c6e87fbf152943f33cb5567f8d6e0d0
Darcko03 <kean1460@gmail.com>
2023-09-17 23:52:38 -0500

    first commit

---
+++ resources/views/booking/create.blade.php
@@ -0,0 +1,34 @@
+@extends('adminlte::page')
+
+@section('template_title')
+    {{ __('Create') }} Booking
+@endsection
+
+@section('content')
+@if (auth()->user()->hasPermissionTo('bookings'))
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+
+                @includeif('partials.errors')
+
+                <div class="card card-default">
+                    <div class="card-header">
+                        <span class="card-title">{{ __('Create') }} Booking</span>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('bookings.store') }}"  role="form" enctype="multipart/form-data">
+                            @csrf
+
+                            @include('booking.form')
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    @else
+    <h3><b>No tienes Permisos para ver esto</b></h3>
+    @endif
+@endsection
